d21ee5add931dc8b93e4c986516c2cec99e00b05
Alexey Ragozin <alexey.ragozin@gmail.com>
2016-02-04 23:17:50 +0400

    Minor fixes

--- src/test/java/org/gridkit/lab/examples/nanocloud/StartingWithDistributedCloud.java
+++ src/test/java/org/gridkit/lab/examples/nanocloud/StartingWithDistributedCloud.java
@@ -12,141 +12,141 @@ import org.junit.Test;
 
 public class StartingWithDistributedCloud extends BaseCloudTest {
 
-	@Test
-	public void test_distributed_hello_world__basic_example() throws InterruptedException {
-		
-		// How cloud will create its nodes is defined by configuration.
-		// Helper method below will create a preconfigured cloud,
-		// where node name is interpreted by hostname.
-		// If you do not have paswordless SSH to that node, 
-		// additional credentials configuration may be required.
-		cloud = CloudFactory.createCloud();
-		RemoteNode.at(cloud.node("**")).useSimpleRemoting();
-		
-		// "cbox1" - "cbox3" are hostnames of VMs I'm using for testing.
-		// You can either put FDQN names of your servers below
-		// or use /etc/hosts to map these short names (as I do).
-		cloud.node("cbox1");
-		cloud.node("cbox2");
-		cloud.node("cbox3");
-		
-		// Optionally you may want to specify java executable.
-		// Default value is "java", so if java is on your PATH you do not need to do it.
-		RemoteNodeProps.at(cloud.node("**")).setRemoteJavaExec("java");
-		
-		// now we have 3 nodes configured to run across two servers
-		// let them say hello
-		
-		// cloud.node("**").touch() will force immediate intialization
-		// of all nodes currently declared in cloud.
-		// It is optional, but it makes console out put less messy
-		cloud.node("**").touch();
-		
-		// Say hello world
-		cloud.node("**").exec(new Callable<Void>() {
-
-			@Override
-			public Void call() throws Exception {
-				String jvmName = ManagementFactory.getRuntimeMXBean().getName();
-				System.out.println("My name is '" + jvmName + "'. Hello!");
-				Thread.sleep(10000);
-				return null;
-			}
-		});
-		
-		// Finally, we will give console output a chance to reach us from remote node.
-		// Console communications are asynchronous, so them may be delayed by few miliseconds. 
-		Thread.sleep(300);
-	}
-
-	@Test
-	public void test_distributed_hello_world__with_node_configuration_tweaking() throws InterruptedException {
-
-		// Continue to use simple SSH cloud
+    @Test
+    public void test_distributed_hello_world__basic_example() throws InterruptedException {
+        
+        // How cloud will create its nodes is defined by configuration.
+        // Helper method below will create a preconfigured cloud,
+        // where node name is interpreted by hostname.
+        // If you do not have paswordless SSH to that node, 
+        // additional credentials configuration may be required.
         cloud = CloudFactory.createCloud();
         RemoteNode.at(cloud.node("**")).useSimpleRemoting();
-		
-		// "cbox1" - "cbox3" are hostnames of VMs I'm using for testing.
-		// You can either put FDQN names of your servers below
-		// or use /etc/hosts to map these short names (as I do).
-		cloud.node("cbox1");
-		cloud.node("cbox2");
-		cloud.node("cbox3");
-		
-		// By default for simple SSH cloud, node name is used as host name.
-		// But imagine that "cbox3" is not available and you want to
-		// host that vi-node on other server.
-		// We can override this by configuring specific vi-node.
+        
+        // "cbox1" - "cbox3" are hostnames of VMs I'm using for testing.
+        // You can either put FDQN names of your servers below
+        // or use /etc/hosts to map these short names (as I do).
+        cloud.node("cbox1");
+        cloud.node("cbox2");
+        cloud.node("cbox3");
+        
+        // Optionally you may want to specify java executable.
+        // Default value is "java", so if java is on your PATH you do not need to do it.
+        RemoteNodeProps.at(cloud.node("**")).setRemoteJavaExec("java");
+        
+        // now we have 3 nodes configured to run across two servers
+        // let them say hello
+        
+        // cloud.node("**").touch() will force immediate intialization
+        // of all nodes currently declared in cloud.
+        // It is optional, but it makes console out put less messy
+        cloud.node("**").touch();
+        
+        // Say hello world
+        cloud.node("**").exec(new Callable<Void>() {
 
-		// RemoteNodeProps is a helper use for convenience.
-		// Same configuration tweaks could be made via .viconf file or setProp(...) method.
-		RemoteNodeProps.at(cloud.node("cbox3"))
-			.setRemoteHost("cbox1")
-			.setRemoteJavaExec("java")
-			.setRemoteJarCachePath("/tmp/extra");
-		
-		// now we have 3 nodes configured to run across two servers
-		// let's verify this be saying hello
-		
-		// Warm up is optional
-		cloud.node("**").touch();
-		
-		// Say hello world
-		cloud.node("**").exec(new Callable<Void>() {
+            @Override
+            public Void call() throws Exception {
+                String jvmName = ManagementFactory.getRuntimeMXBean().getName();
+                System.out.println("My name is '" + jvmName + "'. Hello!");
+                Thread.sleep(10000);
+                return null;
+            }
+        });
+        
+        // Finally, we will give console output a chance to reach us from remote node.
+        // Console communications are asynchronous, so them may be delayed by few miliseconds. 
+        Thread.sleep(300);
+    }
 
-			@Override
-			public Void call() throws Exception {
-				String jvmName = ManagementFactory.getRuntimeMXBean().getName();
-				System.out.println("My name is '" + jvmName + "'. Hello!");
-				return null;
-			}
-		});
-		
-		// You could see that "cbox3" node is actually running on "cbox1" server
+    @Test
+    public void test_distributed_hello_world__with_node_configuration_tweaking() throws InterruptedException {
 
-		// Quick wait to catch up with console
-		Thread.sleep(300);
-	}
-	
-	@Test
-	public void test_distributed_hello_world__with_debug() throws InterruptedException {
-		
-		// Let's create a SSH cloud as in our first example
+        // Continue to use simple SSH cloud
         cloud = CloudFactory.createCloud();
         RemoteNode.at(cloud.node("**")).useSimpleRemoting();
-		
-		cloud.node("cbox1");
-		cloud.node("cbox2");
-						
-		// But now, we would like to debug one of slave processes.
-		// If it could be run on your desktop (no OS dependencies, etc),
-		// you could easy redirect one of slaves to run inside of master JVM.
-		// You can achieve this but using either in-process or isolate node type
-		ViProps.at(cloud.node("cbox1")).setIsolateType();		
-		
-		ViNode allNodes = cloud.node("**");
+        
+        // "cbox1" - "cbox3" are hostnames of VMs I'm using for testing.
+        // You can either put FDQN names of your servers below
+        // or use /etc/hosts to map these short names (as I do).
+        cloud.node("cbox1");
+        cloud.node("cbox2");
+        cloud.node("cbox3");
+        
+        // By default for simple SSH cloud, node name is used as host name.
+        // But imagine that "cbox3" is not available and you want to
+        // host that vi-node on other server.
+        // We can override this by configuring specific vi-node.
 
-		// warming up cluster as usual
-		allNodes.touch();
-		
-		System.out.println("Master JVM name is '" + ManagementFactory.getRuntimeMXBean().getName() + "'");
-		// you can set break point in runnable and catch cbox1.node1 executing it
-		// other vi-nodes are running as separate processes, so they out of reach
-		allNodes.exec(new Callable<Void>() {
+        // RemoteNodeProps is a helper use for convenience.
+        // Same configuration tweaks could be made via .viconf file or setProp(...) method.
+        RemoteNodeProps.at(cloud.node("cbox3"))
+            .setRemoteHost("cbox1")
+            .setRemoteJavaExec("java")
+            .setRemoteJarCachePath("/tmp/extra");
+        
+        // now we have 3 nodes configured to run across two servers
+        // let's verify this be saying hello
+        
+        // Warm up is optional
+        cloud.node("**").touch();
+        
+        // Say hello world
+        cloud.node("**").exec(new Callable<Void>() {
 
-			@Override
-			public Void call() throws Exception {
-				String jvmName = ManagementFactory.getRuntimeMXBean().getName();
-				System.out.println("My name is '" + jvmName + "'. Hello!");
-				return null;
-			}
-		});
-		
-		// You could notice, that output of "in-process" vi-node 
-		// are still prefixed for your convenience.
-		// Same isolation applies to system properties too. 
-		
-		// Quick wait to catch up with console
-		Thread.sleep(300);
-	}	
+            @Override
+            public Void call() throws Exception {
+                String jvmName = ManagementFactory.getRuntimeMXBean().getName();
+                System.out.println("My name is '" + jvmName + "'. Hello!");
+                return null;
+            }
+        });
+        
+        // You could see that "cbox3" node is actually running on "cbox1" server
+
+        // Quick wait to catch up with console
+        Thread.sleep(300);
+    }
+    
+    @Test
+    public void test_distributed_hello_world__with_debug() throws InterruptedException {
+        
+        // Let's create a SSH cloud as in our first example
+        cloud = CloudFactory.createCloud();
+        RemoteNode.at(cloud.node("**")).useSimpleRemoting();
+        
+        cloud.node("cbox1");
+        cloud.node("cbox2");
+                        
+        // But now, we would like to debug one of slave processes.
+        // If it could be run on your desktop (no OS dependencies, etc),
+        // you could easy redirect one of slaves to run inside of master JVM.
+        // You can achieve this but using either in-process or isolate node type
+        ViProps.at(cloud.node("cbox1")).setIsolateType();       
+        
+        ViNode allNodes = cloud.node("**");
+
+        // warming up cluster as usual
+        allNodes.touch();
+        
+        System.out.println("Master JVM name is '" + ManagementFactory.getRuntimeMXBean().getName() + "'");
+        // you can set break point in runnable and catch cbox1.node1 executing it
+        // other vi-nodes are running as separate processes, so they out of reach
+        allNodes.exec(new Callable<Void>() {
+
+            @Override
+            public Void call() throws Exception {
+                String jvmName = ManagementFactory.getRuntimeMXBean().getName();
+                System.out.println("My name is '" + jvmName + "'. Hello!");
+                return null;
+            }
+        });
+        
+        // You could notice, that output of "in-process" vi-node 
+        // are still prefixed for your convenience.
+        // Same isolation applies to system properties too. 
+        
+        // Quick wait to catch up with console
+        Thread.sleep(300);
+    }   
 }
--- src/test/java/org/gridkit/lab/examples/nanocloud/StartingWithLocalCloud.java
+++ src/test/java/org/gridkit/lab/examples/nanocloud/StartingWithLocalCloud.java
@@ -3,110 +3,103 @@ package org.gridkit.lab.examples.nanocloud;
 import java.lang.management.ManagementFactory;
 import java.util.concurrent.Callable;
 
-import org.gridkit.nanocloud.Cloud;
 import org.gridkit.nanocloud.CloudFactory;
+import org.gridkit.nanocloud.VX;
 import org.gridkit.vicluster.ViNode;
-import org.gridkit.vicluster.ViProps;
-import org.gridkit.vicluster.telecontrol.jvm.JvmProps;
 import org.junit.Test;
 
 public class StartingWithLocalCloud extends BaseCloudTest {
 
-	public Cloud createLocalCloud() {
-		Cloud cloud = CloudFactory.createCloud();
-		// this will configure "local" vi-node type by default
-		ViProps.at(cloud.node("**")).setLocalType();
-		return cloud;
-	}
+    @Test
+    public void test_hello_world__version1() {
+        // Let's create simple cloud where slaves will run on same box with master
+        cloud = CloudFactory.createCloud();
+        cloud.node("**").x(VX.TYPE).setLocal();
+        
+        // This line says that 'node1' should exists
+        // all initialization are lazy and asynchronous
+        // so this line will not trigger any process creation
+        cloud.node("node1");
+        
+        // two starts will match any node name
+        ViNode allNodes = cloud.node("**");
+        
+        // let our node to say hello
+        allNodes.exec(new Callable<Void>() {
+        
+            @Override
+            public Void call() throws Exception {
+                String jvmName = ManagementFactory.getRuntimeMXBean().getName();
+                System.out.println("My name is '" + jvmName + "'. Hello!");
+                return null;
+            }
+        });     
+    }
 
-	
-	@Test
-	public void test_hello_world__version1() {
-		// Let's create simple cloud where slaves will run on same box with master
-		cloud = createLocalCloud();
-		
-		// This line says that 'node1' should exists
-		// all initialization are lazy and asynchronous
-		// so this line will not trigger any process creation
-		cloud.node("node1");
-		
-		// two starts will match any node name
-		ViNode allNodes = cloud.node("**");
-		
-		// let our node to say hello
-		allNodes.exec(new Callable<Void>() {
-		
-			@Override
-			public Void call() throws Exception {
-				String jvmName = ManagementFactory.getRuntimeMXBean().getName();
-				System.out.println("My name is '" + jvmName + "'. Hello!");
-				return null;
-			}
-		});		
-	}
+    @Test
+    public void test_hello_world__version2() {
+        cloud = CloudFactory.createCloud();
+        cloud.node("**").x(VX.TYPE).setLocal();
 
-	@Test
-	public void test_hello_world__version2() {
-		cloud = createLocalCloud();
+        // let's create a few more nodes this time
+        cloud.nodes("node1", "node2", "node3", "node4");
+        
+        // say hello
+        cloud.node("**").exec(new Callable<Void>() {
+        
+            @Override
+            public Void call() throws Exception {
+                String jvmName = ManagementFactory.getRuntimeMXBean().getName();
+                System.out.println("My name is '" + jvmName + "'. Hello!");
+                return null;
+            }
+        });     
+    }
 
-		// let's create a few more nodes this time
-		cloud.nodes("node1", "node2", "node3", "node4");
-		
-		// say hello
-		cloud.node("**").exec(new Callable<Void>() {
-		
-			@Override
-			public Void call() throws Exception {
-				String jvmName = ManagementFactory.getRuntimeMXBean().getName();
-				System.out.println("My name is '" + jvmName + "'. Hello!");
-				return null;
-			}
-		});		
-	}
+    @Test
+    public void test_hello_world__version3() throws InterruptedException {
+        cloud = CloudFactory.createCloud();
+        cloud.node("**").x(VX.TYPE).setLocal();
+        
+        cloud.nodes("node1", "node2", "node3", "node4");
+        
+        // let's make sure that all nodes are initialized
+        // before saying 'hello' this time.
+        // touch() will force nodes to be initialized.
+        cloud.node("**").touch();
+        
+        // Console output is pulled asynchronously so we have to give it
+        // few milliseconds to catch up.
+        Thread.sleep(300);
+        
+        // Now we should see quite good chorus
+        cloud.node("**").exec(new Callable<Void>() {
+        
+            @Override
+            public Void call() throws Exception {
+                String jvmName = ManagementFactory.getRuntimeMXBean().getName();
+                System.out.println("My name is '" + jvmName + "'. Hello!");
+                return null;
+            }
+        });     
+    }
 
-	@Test
-	public void test_hello_world__version3() throws InterruptedException {
-		// Let's create simple local cloud first
-		cloud = createLocalCloud();
-		
-		cloud.nodes("node1", "node2", "node3", "node4");
-		
-		// let's make sure that all nodes are initialized
-		// before saying 'hello' this time.
-		// touch() will force nodes to be initialized.
-		cloud.node("**").touch();
-		
-		// Console output is pulled asynchronously so we have to give it
-		// few milliseconds to catch up.
-		Thread.sleep(300);
-		
-		// Now we should see quite good chorus
-		cloud.node("**").exec(new Callable<Void>() {
-		
-			@Override
-			public Void call() throws Exception {
-				String jvmName = ManagementFactory.getRuntimeMXBean().getName();
-				System.out.println("My name is '" + jvmName + "'. Hello!");
-				return null;
-			}
-		});		
-	}
-
-	@Test
-	public void test_jvm_args__version1() throws InterruptedException {
-		cloud = createLocalCloud();
-		
-		// let's create a couple of node1
-		cloud.node("node1");
-		cloud.node("node2");
-		
-		// now let's adjust JVM command line options used to start slave process
-		JvmProps.at(cloud.node("node1")).addJvmArg("-Xms256m").addJvmArg("-Xmx256m");
-		JvmProps.at(cloud.node("node2")).addJvmArg("-Xms512m").addJvmArg("-Xmx512m");
-		
-		cloud.node("**").touch();
-		
-		// Let's see how much memory is available to our slaves
-		reportMemory(cloud);			
-	}		
+    @Test
+    public void test_jvm_args__version1() throws InterruptedException {
+        cloud = CloudFactory.createCloud();
+        cloud.node("**").x(VX.TYPE).setLocal();
+        
+        // let's create a couple of node1
+        cloud.node("node1");
+        cloud.node("node2");
+        
+        // now let's adjust JVM command line options used to start slave process
+        cloud.node("node1").x(VX.PROCESS).addJvmArg("-Xms256m").addJvmArg("-Xmx256m");
+        cloud.node("node2").x(VX.PROCESS).addJvmArgs("-Xms512m", "-Xmx512m");
+        
+        cloud.node("**").touch();
+        
+        // Let's see how much memory is available to our slaves
+        reportMemory(cloud);            
+    }       
 }
